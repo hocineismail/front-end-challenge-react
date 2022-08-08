2bcc6a1d1c704ca167bd05610948ac6a1c14c181
hocineismail <hocineismail75@gmail.com>
2022-08-08 16:33:48 +0100

    Setup frontend's envirement
    Setup Frontend's envirement with Boosttrap instead material ui

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
+
+Enzyme.configure({ adapter: new Adapter() });
+import App from "./App";
+
+/**
+ *Factory function to create a shallowWrapper for App component
+ * @function setup
+ * @param  {object} - Components props specific to this setup
+ * @return {ShallowWrapper}
+ */
+
+const setup = (props = {}) => {
+  return shallow(<App {...props} />);
+};
+
+test("renders App component without errors", () => {
+  const wrapper = setup();
+  const appComponent = wrapper.find({ "data-test-id": "component-app" });
+
+  expect(appComponent.length).toBe(1);
 });
