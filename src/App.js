b7f5dce6c63893f1d58206a096472ad29546a16b
hocineismail <hocineismail75@gmail.com>
2022-08-10 01:29:33 +0100

    Add Navbar-Component
    Add Navbar-Componenet from bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AbsencesManagement from "./components/absencesManagement/AbsencesManagement";
 import Provider from "./context/Provider";
+import { Container, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <Provider>
       <div data-test-id="component-app">
-        <AbsencesManagement />
+        <Navbar style={{ backgroundColor: "#FEA33A" }}>
+          <Container>
+            <Navbar.Brand href="#home">Crewmeiset</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                Signed in as: <b>Ismail hocine</b>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container className="contain">
+          <AbsencesManagement />
+        </Container>
       </div>
     </Provider>
   );
