3409971c47de2818913c2b4e4363a407d6d82723
hocineismail <hocineismail75@gmail.com>
2022-08-10 16:57:33 +0100

    Add multiple rows
    display 10 rows when loading data

--- src/components/loader/LoaderTable.js
+++ src/components/loader/LoaderTable.js
@@ -3,25 +3,237 @@ import TableRow from "../table/TableRow";
 
 export default function LoaderTable() {
   return (
-    <TableRow>
-      <td data-test-id="component-table-cell" style={{ height: "40px" }}>
-        <div className="linear-background"></div>
-      </td>
-      <td data-test-id="component-table-cell" style={{ height: "40px" }}>
-        <div className="linear-background"></div>
-      </td>
-      <td data-test-id="component-table-cell" style={{ height: "40px" }}>
-        <div className="linear-background"></div>
-      </td>
-      <td data-test-id="component-table-cell" style={{ height: "40px" }}>
-        <div className="linear-background"></div>
-      </td>
-      <td data-test-id="component-table-cell" style={{ height: "40px" }}>
-        <div className="linear-background"></div>
-      </td>
-      <td data-test-id="component-table-cell" style={{ height: "40px" }}>
-        <div className="linear-background"></div>
-      </td>
-    </TableRow>
+    <>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+      <TableRow>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+        <td data-test-id="component-table-cell" style={{ height: "40px" }}>
+          <div className="linear-background"></div>
+        </td>
+      </TableRow>
+    </>
   );
 }
