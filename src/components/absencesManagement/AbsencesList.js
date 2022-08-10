a5b005d5a5fd51911f37b5875e2cfa8e69854a8c
hocineismail <hocineismail75@gmail.com>
2022-08-10 14:24:04 +0100

    Add a new  Column 'action'
    from Action Buton allow to confirm, reject and download ics file for each member

--- src/components/absencesManagement/AbsencesList.js
+++ src/components/absencesManagement/AbsencesList.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Alert, Table } from "react-bootstrap";
-
 import TableBody from "../table/TableBody";
 import TableCell from "../table/TableCell";
 import TableHeader from "../table/TableHeader";
 import TableRow from "../table/TableRow";
-
 import { useAbsencesContext } from "../../context/Provider";
 import LoaderTable from "../loader/LoaderTable";
+import AbsencesAction from "./AbsencesAction";
 
-const header = ["Name", "Absence", "Period", "Note", "Status", "Admitter"];
+//header the name of each column on table head
+const header = [
+  "Name",
+  "Absence",
+  "Period",
+  "Note",
+  "Status",
+  "Admitter",
+  "Action",
+];
 
+// AbsencesList is an UI to display the table of members
 export default function AbsencesList() {
   // useReducer for sharing state between components
   const { state, onFetchAbsences } = useAbsencesContext();
@@ -40,10 +49,7 @@ export default function AbsencesList() {
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row) => {
                   return (
-                    <TableRow
-                      data-test-id="table-absences-rows"
-                      key={row.crewId}
-                    >
+                    <TableRow data-test-id="table-absences-rows" key={row.id}>
                       <TableCell
                         data-test-id="table-absences-col"
                         item={row.name}
@@ -63,11 +69,24 @@ export default function AbsencesList() {
                       <TableCell
                         data-test-id="table-absences-col"
                         item={row.status}
+                        badge={row.status}
                       />
                       <TableCell
                         data-test-id="table-absences-col"
                         item={row.admitterNote}
                       />
+                      <TableCell data-test-id="table-absences-col">
+                        <AbsencesAction
+                          status={row.status}
+                          absenceId={row.id}
+                          ics={{
+                            title: `The Absences of ${row.name}`,
+                            description: `Absence of  ${row.name}, type:  ${row.type} `,
+                            startTime: new Date(row.endDate),
+                            endTime: new Date(row.endDate),
+                          }}
+                        />
+                      </TableCell>
                     </TableRow>
                   );
                 })}
