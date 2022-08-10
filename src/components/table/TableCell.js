aee690d9191b8a87ce84e2c81032110b74a4ca3e
hocineismail <hocineismail75@gmail.com>
2022-08-10 15:53:52 +0100

    Add children to TableCell
    Add the ability to  use children on td table when we want to use buttons or any component

--- src/components/table/TableCell.js
+++ src/components/table/TableCell.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Badge from "../badge/Badge";
 
-export default function TableCell({ item, badge }) {
+export default function TableCell({ item, badge, children }) {
   return (
     <td data-test-id="component-table-cell">
-      {badge ? (
+      {children ? (
+        children
+      ) : badge ? (
         <Badge item={item} className={`rc-badge badge-${badge}`} />
       ) : item ? (
         item
